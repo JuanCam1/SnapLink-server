41f773f5914bf822241090b7ffc6a0b16b785bd7
Juan Camilo Rojas <juancamilor576@gmail.com>
2025-07-16 22:40:33 -0500

    feat: agregado router link a la lista de rutas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "../app/auth/auth-route";
+import linkRouter from "../app/link/link-route";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/link", linkRouter);
 
 export default router;
